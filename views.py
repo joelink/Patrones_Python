f4cd84045680f5c9050bbda6ac3f9eaa79336b29
Etherias10 <33073520+Etherias10@users.noreply.github.com>
2017-10-24 16:52:20 -0600

    Add files via upload

---
+++ views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+from .models import Album
+
+# Create your views here.
+
+def index(request):
+          return HttpResponse("<h1>This is the New Music app home page </h1>")
+
+def details(request, album_id):
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/details.html', {'album': album,})
+
+def list(request):
+    all_Albums = Album.objects.all()
+    # Se puede crear el diccionario con una variable como en este caso o
+    # simplemtene como es solo y objeto entonces llamarlo en el return
+    context = {'all_Albums': all_Albums,}
+    return render(request, 'music/Album_list.html', context)
+#esto no es java
